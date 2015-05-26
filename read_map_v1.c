08d6a31c1b121397ab1c506dd4baa1de60fd2f65
Jeronimo <jeronimo.pellegrini@ufabc.edu.br>
2015-05-26 16:17:16 -0300

    * added map and map-reading prototype

---
+++ read_map_v1.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+
+/*
+  This file only reads and writes the map from
+  a file called "map.txt"
+*/
+int main(void) {
+        char url[]="map.txt";
+        FILE *arq;
+        arq = fopen(url, "r");
+        if(arq == NULL)
+            printf("Erro, nao foi possivel abrir o arquivo \n");
+        else {
+                int i = 0, j = 0;
+		/* TODO: following line is a temporary
+		   hack */
+                char map[20][20];
+                char ch;
+                while( (ch=fgetc(arq))!= EOF ){
+                        map[i][j] = ch;
+                        printf("%c", map[i][j]);
+                        i++;
+                        if(ch == '\n'){
+                                i = 0;
+                                j ++;   
+                        }
+                }
+        }
+        fclose(arq);
+        return 0;
+}
+
+
+
