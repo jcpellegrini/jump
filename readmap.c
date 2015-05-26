bad023298cf32cb0dad3e46e1fa3e0452898381b
felipeanchieta <felipe.anchieta@aluno.ufabc.edu.br>
2015-05-26 17:10:27 -0300

    Função de leitura de mapa adicionada. Sintaxe no arquivo.

---
+++ readmap.c
@@ -0,0 +1,63 @@
+/*  Função ReadFile
+ *  Sintaxe: char** readmap(char* url)
+ *  Descrição: abre um arquivo de texto especificado e retorna sua matriz de caracteres
+ *  
+ *  Autor: Felipe Anchieta Santos Costa
+ *
+ *  A implementar: leitura de mapas com dimensão no topo do arquivo :)
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define NLINHAS 20
+#define NCOLUNA 20
+
+
+char** readmap(char* url) {
+    
+    FILE *arq = fopen(url, "r");
+    
+    if(!arq) { 
+        
+        printf("Erro, não foi possivel abrir o arquivo\n");
+        return NULL;
+        
+    } else {
+        int i = 0, j = 0;
+        char ch;
+        char **map = malloc(NLINHAS*sizeof(char *));
+        
+        if(!map) printf("Sem memória suficiente\n");
+        else {
+            for(; i < NLINHAS; i++) {
+                map[i] = malloc(NCOLUNA*sizeof(char));
+                
+                if(!map[i]) {
+                    printf("Sem memória suficiente\n");
+                    break;
+                }
+            }
+        }
+        
+        i = 0;
+            
+        while( (ch=fgetc(arq))!= EOF ) {
+            
+            map[i][j] = ch;
+            
+            i++;
+            if(ch == '\n'){
+                i = 0;
+                j++;   
+            }
+        }
+        
+        fclose(arq);
+        return map;
+    }
+    
+    
+}
+
+
